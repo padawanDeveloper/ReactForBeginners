6c1c3a119aca75078d8f1c25eaf4be8a833ea09a
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-17 17:38:53 -0300

    add details from props to menu-fish

---
+++ src/components/Fish.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {formatPrice} from "../helpers";
+
+class Fish extends React.Component {
+  render() {
+      const { image, name, price, desc, status }= this.props.details;
+    return (
+        <li className="menu-fish">
+            <img src={image} alt={name}/>
+            <h3 className="fish-name">
+                {name}
+                {/* Utilizamos la funcion formatPrice que se encuentra en helpers y le pasamos el valos price*/}
+                <span className="price">{formatPrice(price)}</span>
+                
+            </h3>
+            <p>{desc}</p>
+            <button>Add To Cart</button>
+        </li>
+    );
+  }
+}
+
+export default Fish;
