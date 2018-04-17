5fdb4dc82fb173ab58da29cfb3ef5a1581aadd34
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-17 18:46:20 -0300

    refactor add to order

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,23 @@ import React from "react";
 import {formatPrice} from '../helpers';
 
 class Order extends React.Component {
+  renderOrder = (key)=> {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === 'available';
+    if(!isAvailable) {
+      <li>
+        Sorry {fish ? fish.name : 'fish'} is no longer available
+      </li>
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
@@ -17,7 +34,7 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        {orderIds}
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
